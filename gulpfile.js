9bacb02a5b21d08c9a181afcdb7e3dbcd616e5c5
thallesmarchetti <linkmarchetti@gmail.com>
2018-03-21 00:51:27 -0300

    add error notify on tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const   browserSync =   require('browser-sync'),
         babel       =   require('gulp-babel'),
@@ -10,58 +10,99 @@ const   browserSync =   require('browser-sync'),
         gulpif      =   require('gulp-if'),
         gutil       =   require('gulp-util'),
         imagemin    =   require('gulp-imagemin'),
+        message     =   require('./src/message'),
         minifyHtml  =   require('gulp-minify-html'),
         nunjucks    =   require('gulp-nunjucks-html'),
         plumber     =   require('gulp-plumber'),
         stylus      =   require('gulp-stylus'),
         uglify      =   require('gulp-uglify');
 
-// Nunjucks
+/*
+|--------------------------------------------------------------------------
+| Nunjucks Tasks
+|--------------------------------------------------------------------------
+*/
 gulp.task('nunjucks', () => {
     return gulp.src('src/templates/*.html')
         .pipe(plumber())
         .pipe(nunjucks({
             searchPaths: ['src/templates/']
         }))
+        .on('error', message.error('NUNJUCKS: Compilation'))
         .pipe(gulpif(env.p, minifyHtml()))
         .pipe(gulp.dest('build/'));
 });
-
-// Stylus
+/*
+|--------------------------------------------------------------------------
+| Stylus Tasks
+|--------------------------------------------------------------------------
+*/
 gulp.task('stylus', () => {
-    gulp.src('src/styl/main.styl')
-    .pipe(plumber())
-    .pipe(stylus({compress: true}))
-    .pipe(gulp.dest('build/css'));
+    gulp.src("src/styl/main.styl")
+        .pipe(plumber())
+        .pipe(stylus({compress: true }))
+        .pipe(gulp.dest("build/css"));
 });
-
-// Uglify and Concat JS
-gulp.task('js', () => {
+/*
+|--------------------------------------------------------------------------
+| Uglify and Concat JS
+|--------------------------------------------------------------------------
+*/
+gulp.task('javascript', () => {
     return gulp.src('src/js/**/*.js')
         .pipe(plumber())
         .pipe(babel({presets: ['env']}))
+        .on('error', message.error("JAVASCRIPT: Bundling"))
         .pipe(uglify())
+        .on('error', message.error('JAVASCRIPT: Minification'))
         .pipe(concat('scripts.js'))
         .pipe(gulp.dest('build/js/'))
 });
-
-// Imagemin
+/*
+|--------------------------------------------------------------------------
+| Images Tasks
+|--------------------------------------------------------------------------
+*/
 gulp.task('images', () => {
     gulp.src('src/img/**/*')
         .pipe(plumber())
         .pipe(imagemin({interlaced: true, progressive: true, optimizationLevel: 2}))
+        .on('error', message.error('IMAGE: Minification'))
         .pipe(gulp.dest('build/img'));
 });
-
-// Watch
+/*
+|--------------------------------------------------------------------------
+| Watching Tasks
+|--------------------------------------------------------------------------
+|
+| They look for the files changes and runs building tasks.
+| We're watching each domain and recompiling separetly
+| for better performance.
+|
+*/
 gulp.task('watch', () => {
-    gulp.watch('src/templates/**/*.html', ['nunjucks']);
-    gulp.watch('src/styl/**/*.styl', ['stylus']);
-    gulp.watch('src/js/**/*.js', ['js']);
-    gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
-});
+    gulp.watch('src/templates/**/*.html', ['nunjucks'])
+    .on('error', message.error('WATCH: Views'));
 
-// Browsesync
+    gulp.watch('src/styl/**/*.styl', ['stylus'])
+    .on('error', message.error('WATCH: Stylus'));
+
+    gulp.watch('src/js/**/*.js', ['javascript'])
+    .on('error', message.error('WATCH: Javascript'));
+
+    gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin'])
+    .on('error', message.error('WATCH: Images'));
+});
+/*
+|--------------------------------------------------------------------------
+| Browsesync
+|--------------------------------------------------------------------------
+|
+| BrowserSync and starts a localhost development. Compiled
+| files are outputted into `build` directory, so we are
+| telling BrowserSync to to use it as a base.
+|
+*/
 gulp.task('browser-sync', () => {
    let files = [
       'build/**/*.html',
@@ -78,19 +119,29 @@ gulp.task('browser-sync', () => {
       }
    });
 });
-
-// ghpages
+/*
+|--------------------------------------------------------------------------
+| ghpages Task
+|--------------------------------------------------------------------------
+*/
 gulp.task('pages', () => {
     return gulp.src('./build/**/*')
     .pipe(
         ghPages({
           branch: 'master',
-          cacheDir: '.deploy'
+          cacheDir: '.build'
         })
       )
 });
-
-// default
-gulp.task('default', ['nunjucks', 'js', 'stylus', 'images', 'watch', 'browser-sync']);
-// deploy-gh
-gulp.task('deploy-gh', ['nunjucks', 'js', 'stylus', 'images', 'pages']);
+/*
+|--------------------------------------------------------------------------
+| default task
+|--------------------------------------------------------------------------
+*/
+gulp.task('default', ['nunjucks', 'stylus', 'javascript', 'images', 'watch', 'browser-sync']);
+/*
+|--------------------------------------------------------------------------
+| deploy
+|--------------------------------------------------------------------------
+*/
+gulp.task('deploy', ['nunjucks', 'javascript', 'stylus', 'images', 'pages']);
